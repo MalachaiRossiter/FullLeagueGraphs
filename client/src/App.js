9f8526e78274c1acb45dab76064767cbecaa08f8
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-04-01 11:26:09 -0400

    gets complete list of champions from summoner

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const [summonerName, setSummonerName] = useState();
   const [summonerId, setSummonerId] = useState();
-  const [summonerMastery, setSummonerMastery] = useState();
+  const [summonerMastery, setSummonerMastery] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -17,27 +17,35 @@ function App() {
       setSummonerName(res.data.name);
       setSummonerId(res.data.id);
       console.log(summonerName);
-
-      // get champion masteries by summoner name if account is valid
-      // axios.get(`https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summonerId}?api_key=${process.env.REACT_APP_API_KEY}`)
-      // .then(res => {
-      //   setSummonerMastery(res.data);
-      //   console.log(summonerMastery);
-      //   setLoaded(true);
-      // })
-      // .catch((err) => console.log(err));
+      console.log(summonerId);
     })
     .catch((err) => console.log(err));
   }, [])
+
+  useEffect(() => {
+    if (!summonerId) {
+      return;
+    }
+  
+    axios.get(`http://localhost:8000/api/summonerMastery/${summonerId}`)
+    .then(res => {
+      console.log(res.data);
+      const masteryIds = res.data.map(mastery => mastery.championId);
+      setSummonerMastery(masteryIds);
+      setLoaded(true);
+    })
+    .catch((err) => console.log(err));
+  }, [summonerId]);
+  
 
   return (
     <div className="App">
       <h1>Wow this is cool!</h1>
       <p>{summonerName}</p>
       {
-        loaded && summonerMastery.map((champion) => (
-          <div key={champion}>
-            <img src={`/assets/${champion.id}`} alt={'champion image here'}/>
+        loaded && summonerMastery.map((championId) => (
+          <div key={championId}>
+            <img src={`/assets/${championId}.png`} alt={'champion image here'}/>
           </div>
         ))
       }
--- server/server.js
+++ server/server.js
@@ -26,6 +26,19 @@ app.get('/api/summoner/:name', async (req, res) => {
     res.json(data);
 });
 
+app.get('/api/summonerMastery/:summonerId', async (req, res) => {
+    let url = `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${req.params.summonerId}?api_key=${process.env.API_KEY}`
+    console.log(url);
+    const response = await fetch(url, {
+        headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:3000'
+        }
+    });
+
+    const data = await response.json();
+    res.json(data);
+});
+
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 });
