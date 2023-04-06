d25b262a5371126b6c5d7d67b99bc27390fdd7f1
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-04-05 23:59:21 -0400

    something

--- client/src/App.js
+++ client/src/App.js
@@ -10,40 +10,40 @@ function App() {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    // get summoner by account name
-    axios.get(`http://localhost:8000/api/summoner/FierDragon`)
-    .then(res => {
-      console.log(res);
-      setSummonerName(res.data.name);
-      setSummonerId(res.data.id);
-      console.log(summonerName);
-      console.log(summonerId);
-    })
-    .catch((err) => console.log(err));
-  }, [])
-
-  useEffect(() => {
     if (!summonerId) {
       return;
     }
-  
-    axios.get(`http://localhost:8000/api/summonerMastery/${summonerId}`)
-    .then(res => {
-      console.log(res.data);
-      const masteryIds = res.data.map(mastery => mastery.championId);
-      setSummonerMastery(masteryIds);
-      setLoaded(true);
-    })
-    .catch((err) => console.log(err));
+
   }, [summonerId]);
   
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // get summoner by account name
+    axios.get(`http://localhost:8000/api/summoner/${summonerName}`)
+      .then(res => {
+        console.log(res);
+        setSummonerId(res.data.id);
+        console.log(summonerName);
+        console.log(res.data.id); // log the id from the response instead of the state variable
+        axios.get(`http://localhost:8000/api/summonerMastery/${res.data.id}`) // use the id from the response
+          .then(res => {
+            console.log(res.data);
+            const masteryIds = res.data.map(mastery => mastery.championId);
+            setSummonerMastery(masteryIds);
+            setLoaded(true);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div className="App">
       <h1>Wow this is cool!</h1>
       <p>{summonerName}</p>
-      <form onSubmit={}>
-
+      <form onSubmit={submitHandler}>
+        <input type="text" value={summonerName} onChange={(e) => setSummonerName(e.target.value)} />
+        <input type='submit'/>
       </form>
       {
         loaded && summonerMastery.map((championId) => (
